489499f17aeef52c66eb022503b8b27e07a61d9b
Gosia Korpik <malgorzata.korpik10@gmail.com>
2019-07-23 17:53:45 +0200

    Add playerMove - paper and scissors

--- js/script.js
+++ js/script.js
@@ -42,10 +42,27 @@ printMessage('Wylosowana liczba to: ' + randomNumber);
 console.log('wylosowana liczba to: ' + randomNumber);
 
 if (randomNumber == '1') {
-  computerMove = 'kamień';
+  computerMove = stone;
 } else if (randomNumber == '2') {
 	computerMove = paper;
 } else {
   computerMove = scissors;
 }
 printMessage('Mój ruch: ' + computerMove);
+
+
+// ruch gracza
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = stone;
+} else if (playerInput == '2') {
+	playerMove = paper;
+} else if (playerInput =='3') {
+	playerMove = scissors;
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
